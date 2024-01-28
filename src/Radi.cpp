1364a0ca1be22b4f3904bb0de8580aa2d0b665b1
Robert-W-Ward <robertwesleyward2019@gmail.com>
2024-01-27 19:10:54 -0600

    Make changes to git ignore

---
+++ src/Radi.cpp
@@ -0,0 +1,11 @@
+﻿// Radi.cpp : Defines the entry point for the application.
+//
+
+#include "../include/Radi.h"
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
