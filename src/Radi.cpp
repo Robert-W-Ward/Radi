dea61ed58f63a0f3c43cfa38984a859b83070c02
Robert-W-Ward <robertwesleyward2019@gmail.com>
2024-01-28 01:13:31 -0600

    Fixed the conan build issues

--- src/Radi.cpp
+++ src/Radi.cpp
@@ -1,11 +1,35 @@
-﻿// Radi.cpp : Defines the entry point for the application.
-//
-
+﻿#include "GLFW/glfw3.h"
 #include "../include/Radi.h"
-using namespace std;
 
-int main()
-{
-	cout << "Hello CMake." << endl;
-	return 0;
+int main() {
+    // Initialize GLFW
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    // Create a windowed mode window and its OpenGL context
+    GLFWwindow* window = glfwCreateWindow(500, 500, "Hello GLFW", NULL, NULL);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // Make the window's context current
+    glfwMakeContextCurrent(window);
+
+    // Loop until the user closes the window
+    while (!glfwWindowShouldClose(window)) {
+        // Render here
+
+        // Swap front and back buffers
+        glfwSwapBuffers(window);
+
+        // Poll for and process events
+        glfwPollEvents();
+    }
+
+    glfwTerminate(); // Clean up and close the window
+    return 0;
 }
