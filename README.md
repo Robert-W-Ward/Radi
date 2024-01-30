62b1389b9c0793030c023f29440686e451a8cf1b
Robert-W-Ward <robertwesleyward2019@gmail.com>
2024-01-29 22:30:30 -0600

    Added GL Extensions, edited readme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Radi
+A simple 2D & 3D Renderer build with OpenGL
--- src/Radi.cpp
+++ src/Radi.cpp
@@ -1,15 +1,26 @@
-﻿#include "GLFW/glfw3.h"
+﻿#include "glad/glad.h"
+#include "GLFW/glfw3.h"
 #include "../include/Radi.h"
 
+
+const int VIEWPORT_X = 800;
+const int VIEWPORT_Y = 600;
+const int WINDOW_X = VIEWPORT_X;
+const int WINDOW_Y = VIEWPORT_Y;
 int main() {
     // Initialize GLFW
     if (!glfwInit()) {
         std::cerr << "Failed to initialize GLFW" << std::endl;
+        glfwTerminate();
         return -1;
     }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
 
     // Create a windowed mode window and its OpenGL context
-    GLFWwindow* window = glfwCreateWindow(500, 500, "Hello GLFW", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(WINDOW_X, WINDOW_Y, "Hello GLFW", NULL, NULL);
     if (!window) {
         std::cerr << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
@@ -19,6 +30,12 @@ int main() {
     // Make the window's context current
     glfwMakeContextCurrent(window);
 
+    //Init glad so that we can Access OpenGL function pointers
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    glViewport(0, 0, 800, 600);
     // Loop until the user closes the window
     while (!glfwWindowShouldClose(window)) {
         // Render here
